4ebdaddade5ac3aa0f0b8779c0b19f93090bfba7
jeffjack38 <jeaden38@gmail.com>
2023-10-12 20:46:17 -0500

    Added entities to Owner/getters/setters/id

--- src/model/Owner.java
+++ src/model/Owner.java
@@ -5,22 +5,73 @@
  */
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table()// database and table name
 public class Owner {
+
+	@Id
+	@GeneratedValue
+	@Column(name="ID")
+	private int id;
+	@Column(name="NAME")
     private String name;
+	@Column(name="CONTACTNUMBER")
     private String contactNumber;
+	
+	public Owner() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
 
     public Owner(String name, String contactNumber) {
         this.name = name;
         this.contactNumber = contactNumber;
     }
 
-    // Getters and setters for all fields - dustin add these
 
-    @Override
-    public String toString() {
-        return "Owner{" +
-                "name='" + name + '\'' +
-                ", contactNumber='" + contactNumber + '\'' +
-                '}';
-    }
+
+    public Owner(int id, String name, String contactNumber) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.contactNumber = contactNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Owner [id=" + id + ", name=" + name + ", contactNumber=" + contactNumber + "]";
+	}
+
+	
 }
